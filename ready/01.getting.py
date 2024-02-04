a9c82065ebbc3121a9ab48f08fdccdc5c001d338
ehdrn3020 <ehdrnsla3020@naver.com>
2024-02-04 16:30:42 +0900

    주석추가

--- ready/01.getting.py
+++ ready/01.getting.py
@@ -12,9 +12,6 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # 데이터 로드, 데이터는 \t을 기준으로 구분
 df = pd.read_csv("https://raw.githubusercontent.com/Datamanim/pandas/main/lol.csv", sep="\t")
 
-# 데이터 로드, 한글데이터(utf-8 > cp949 > euc-kr > latin)
-df_kr = pd.read_csv("https://raw.githubusercontent.com/Datamanim/pandas/main/Jeju.csv", encoding='euc-kr')
-
 # 제공 메소드 확인
 dir(df) # help(df)
 
@@ -34,6 +31,10 @@ df[df.columns[1]].dtype
 df[df.columns[2]][3] # 인덱스는 0부터 시작
 df.iloc[3,2] # [행, 열]
 
+
+# 데이터 로드, 한글데이터(utf-8 > cp949 > euc-kr > latin)
+df_kr = pd.read_csv("https://raw.githubusercontent.com/Datamanim/pandas/main/Jeju.csv", encoding='euc-kr')
+
 # 3번째 컬럼의 10~20번째 값
 df_kr.iloc[10:20,4]
 
@@ -46,5 +47,5 @@ df_kr.isnull().sum()
 # 수치형변수의 분포(사분위, 평균, 표준편차, 최대, 최소)를 확인
 df_kr.describe()
 
-# 읍면동명의 유일 값을 출력
+# 읍면동명의 유일 값을 출력 (series)
 df_kr['읍면동명'].unique()
